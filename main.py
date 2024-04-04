cf9c4d409cb49ebbf35b4f24799983b6b2a15588
vsvivek12 <165539237+vsvivek12@users.noreply.github.com>
2024-04-04 10:55:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import streamlit as st
 import pandas as pd
 
 name= st.text_input("Enter your name here:")
-fname =st.text_input("enter your fathers name here:")
+fname =st.text_input("Enter your fathers name here:")
 adr = st.text_area('Enter your address:')
-classdata = st.selectbox("Enter your class :",(1,2,3,4,5,6,))
+classdata = st.selectbox("Enter your semester :",(1,2,3,4,5,6,))
 button =st.button("Done")
 
 if button:
